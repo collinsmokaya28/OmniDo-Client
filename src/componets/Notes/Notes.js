eaa19bab91d5d18d4a38873c7fb0854878856762
Juspher Mokua <juspermokua6@gmail.com>
2022-11-09 00:07:42 +0300

    done

--- src/componets/Notes/Notes.js
+++ src/componets/Notes/Notes.js
@@ -1,9 +1,43 @@
 import React from 'react'
 
-function Notes() {
-    return (
-        <div>Notes</div>
-    )
-}
+function Notes(props) {
+    const updateTitle = (e) => {
+        const updatedValue = e.target.value;
+        const editMeId = props.note.id;
+        props.onType(editMeId, "title", updatedValue);
+      };
+    
+      const updateDescription = (e) => {
+        const updatedValue = e.target.value;
+        const editMeId = props.note.id;
+        props.onType(editMeId, "description", updatedValue);
+      };
+    
+      const clickDelete = () => {
+        props.removeNote(props.note.id);
+      };
+    
+      return (
+        <li className="Notes">
+          <input
+            className="Notes__title"
+            type="text"
+            placeholder="Title"
+            value={props.Notes.title}
+            onChange={updateTitle}
+          />
+          <textarea
+            className="Notes__description"
+            placeholder="Description..."
+            value={props.Notes.description}
+            onChange={updateDescription}
+          />
+          <span className="Notes__delete" onClick={clickDelete}>
+            X
+          </span>
+        </li>
+      );
+    };
+    
+    export default Notes;
 
-export default Notes
