490f3496a1d19b840730899379daef3b6043a158
Felix <barosiofelix@gmail.com>
2022-11-08 22:20:19 +0300

    Updated Routes

--- src/componets/NavBar.js
+++ src/componets/NavBar.js
@@ -5,10 +5,10 @@ function NavBar() {
     return (
         <div className='header'>
             <div className='navbar-container'>
-                <Link className="navbar-logo" to="/">My To-Do-App</Link>
+                <Link className="navbar-logo" to="/">To-Do-App</Link>
                 <div className='nav-menu'>
                     <Link className='nav-links' to="/notes">Notes</Link>
-                    <Link className='nav-links' to="/">ToDo</Link>
+                    <Link className='nav-links' to="/todo">ToDo</Link>
                     <Link className='nav-links' to="/reminder">Reminder</Link>
                     <Link className='nav-links' to="/login">Login</Link>
                 </div>
