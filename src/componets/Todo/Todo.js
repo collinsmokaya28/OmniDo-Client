011ce9b0bb58c2dd121b45eafd68fb8b79e088a5
Felix <barosiofelix@gmail.com>
2022-11-09 11:49:24 +0300

    Added return

--- src/componets/Todo/Todo.js
+++ src/componets/Todo/Todo.js
@@ -1,11 +1,25 @@
-import React from 'react'
+import { BsCheckLg, BsFillTrashFill, BsXSquareFill } from "react-icons/bs";
+import React from "react";
 
-function Todo() {
+function Todo({ todo, onItemUpdate, onItemDelete }) {
     return (
         <div>
-            My To Do
+            <li className={todo.done ? "done" : ""}>
+                <span>{todo.text}</span>
+                <span className="category">{todo.category}</span>
+                <button className="btn check" onClick={handleDone}>
+
+                    {todo.done ? <BsCheckLg /> : <BsXSquareFill />}
+
+                </button>
+                <button className="remove btn" onClick={handleDelete}>
+
+                    <BsFillTrashFill />
+
+                </button>
+            </li>
         </div>
-    )
+    );
 }
 
-export default Todo
+export default Todo;
