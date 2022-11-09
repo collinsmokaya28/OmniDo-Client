071dc1cf5fe7ada1bf06a2b700fd2b307b78a209
Juspher Mokua <juspermokua6@gmail.com>
2022-11-09 11:24:23 +0300

    done

--- src/componets/Notes/Notes.js
+++ src/componets/Notes/Notes.js
@@ -1,43 +1,73 @@
-import React from 'react'
+import React, { useMemo, useState } from "react";
+import { v4 as uuidv4 } from "uuid";
 
-function Notes(props) {
-    const updateTitle = (e) => {
-        const updatedValue = e.target.value;
-        const editMeId = props.note.id;
-        props.onType(editMeId, "title", updatedValue);
-      };
-    
-      const updateDescription = (e) => {
-        const updatedValue = e.target.value;
-        const editMeId = props.note.id;
-        props.onType(editMeId, "description", updatedValue);
-      };
-    
-      const clickDelete = () => {
-        props.removeNote(props.note.id);
-      };
-    
-      return (
-        <li className="Notes">
+function Notes() {
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+  const [keyword, setKeyword] = useState("");
+  const [notes, setNotes] = useState([]);
+  const add = (e) => {
+    e.preventDefault();
+    setNotes((notes) => [
+      ...notes,
+      {
+        id: uuidv4(),
+        title,
+        description
+      }
+    ]);
+  };
+  const remove = (index) => {
+    setNotes((notes) => notes.filter((_, i) => i !== index));
+  };
+  const filteredNotes = useMemo(() => {
+    if (!keyword) {
+      return notes;
+    }
+    return notes.filter(({ title, description }) => {
+      return title.includes(keyword) || description.includes(keyword);
+    });
+  }, [keyword, notes]);
+
+  return (
+    <div>
+        <h1>Notes</h1>
+        <form onSubmit={add}>
+        <h1>add note</h1>
+        <div>
+          <label>title</label>
+          <input value={title} onChange={(e) => setTitle(e.target.value)} />
+        </div>
+        <div>
+          <label>description</label>
           <input
-            className="Notes__title"
-            type="text"
-            placeholder="Title"
-            value={props.Notes.title}
-            onChange={updateTitle}
+            value={description}
+            onChange={(e) => setDescription(e.target.value)}
           />
-          <textarea
-            className="Notes__description"
-            placeholder="Description..."
-            value={props.Notes.description}
-            onChange={updateDescription}
-          />
-          <span className="Notes__delete" onClick={clickDelete}>
-            X
-          </span>
-        </li>
-      );
-    };
-    
-    export default Notes;
+        </div>
+        <button type="submit">add</button>
+      </form>
+      <form>
+        <h1>search</h1>
+        <div>
+          <label>keyword</label>
+          <input value={keyword} onChange={(e) => setKeyword(e.target.value)} />
+        </div>
+      </form>
+      {filteredNotes.map((note, index) => {
+        return (
+          <div key={note.id}>
+            <h2>{note.title}</h2>
+            <p>{note.description}</p>
+            <button type="button" onClick={() => remove(index)}>
+              remove
+            </button>
+          </div>
+        );
+      })}
 
+    </div>
+  )
+}
+
+export default Notes
