5220d3b7f9544f936637ce31bc74a1a2a6b13144
Felix <barosiofelix@gmail.com>
2022-11-09 14:17:28 +0300

    Modified work on the ft-reminder branch

--- src/componets/Reminder/Reminder.js
+++ src/componets/Reminder/Reminder.js
@@ -1,11 +1,47 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
+import { useState } from 'react'
+import { data } from './Birthday-data'
 import './Reminder.css'
-import App from './App'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-)
+function Reminder() {
+  const [people, setPeople] = useState(data)
+
+  const removePerson = (id) => {
+    let newPerson = people.filter((person) => person.id !== id)
+    setPeople(newPerson)
+  }
+
+  return (
+    <>
+      <h3
+        style={{
+          backgroundColor: '#fff',
+          color: '#313174',
+          textAlign: 'center',
+          fontSize: '36px',
+          marginTop: '70px'
+        }}
+      >
+        {people.length} Todays Birthdays
+      </h3>
+      {people.map((person) => {
+        const { id, name, image } = person
+
+        return (
+          <div className='container' key={id}>
+            <div className='avatar'>
+              <img src={image} alt={name} />
+              <h3>{name}</h3>
+            </div>
+            <div className='button'>
+              <button className='btn' onClick={() => removePerson(id)}>
+                Delete
+              </button>
+            </div>
+          </div>
+        )
+      })}
+    </>
+  )
+}
+
+export default Reminder;
