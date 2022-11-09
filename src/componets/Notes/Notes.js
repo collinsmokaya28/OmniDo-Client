8e6d3fa16cde91b65a2832025e6a926bc7dc644d
Felix <barosiofelix@gmail.com>
2022-11-09 18:54:49 +0300

    Made changes to ft-notes

--- src/componets/Notes/Notes.js
+++ src/componets/Notes/Notes.js
@@ -31,41 +31,43 @@ function Notes() {
     });
   }, [keyword, notes]);
   return (
-    <div className="notesdetails" >
+    <div className="main-cont container">
+      <div className="notesdetails" >
         <h1>Notes</h1>
         <form className="notesform" onSubmit={add}>
-        <h1>Add note</h1>
-        <div className="forminput">
-          <label>Title</label>
-          <input value={Title} onChange={(e) => setTitle(e.target.value)} />
-        </div>
-        <div className="forminput">
-          <label>Description</label>
-          <input
-            value={Description}
-            onChange={(e) => setDescription(e.target.value)}
-          />
-        </div>
-        <button className="addbtn" type="submit">Add</button>
-      </form>
-      <form className="searchform">
-        <h1>Search</h1>
-        <div>
-          <label>Keyword</label>
-          <input value={keyword} onChange={(e) => setKeyword(e.target.value)} />
-        </div>
-      </form>
-      {filteredNotes.map((note, index) => {
-        return (
-          <div key={note.id}>
-            <h2>{note.Title}</h2>
-            <p>{note.Description}</p>
-            <button type="button" onClick={() => remove(index)}>
-              Remove
-            </button>
+          <h1>Add note</h1>
+          <div className="forminput">
+            <label>Title</label>
+            <input value={Title} onChange={(e) => setTitle(e.target.value)} />
           </div>
-        );
-      })}
+          <div className="forminput">
+            <label>Description</label>
+            <input
+              value={Description}
+              onChange={(e) => setDescription(e.target.value)}
+            />
+          </div>
+          <button className="addbtn" type="submit">Add</button>
+        </form>
+        <form className="searchform">
+          <h1>Search</h1>
+          <div>
+            <label>Keyword</label>
+            <input value={keyword} onChange={(e) => setKeyword(e.target.value)} />
+          </div>
+        </form>
+        {filteredNotes.map((note, index) => {
+          return (
+            <div key={note.id}>
+              <h2>{note.Title}</h2>
+              <p>{note.Description}</p>
+              <button type="button" onClick={() => remove(index)}>
+                Remove
+              </button>
+            </div>
+          );
+        })}
+      </div>
     </div>
   )
 }
