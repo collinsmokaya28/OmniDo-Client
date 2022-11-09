ea5311ab657935cfcbf4fd2b5535f1358d260ed8
Felix <barosiofelix@gmail.com>
2022-11-09 11:52:02 +0300

    Added fetch function

---
+++ src/componets/Todo/TodoList.js
@@ -0,0 +1,47 @@
+import React, { useEffect, useState } from "react";
+import NewTodo from "./NewTodo";
+import Sort from "./Sort";
+import Todo from "./Todo";
+import './Todo.css';
+
+function TodoList() {
+
+    const [selectedCategory, setSelectedCategory] = useState("All");
+    const [todos, setTodos] = useState([]);
+
+    useEffect(() => {
+        fetch("http://localhost:9292/todos")
+            .then((res) => res.json())
+            .then((data) => setTodos(data));
+    }, []);
+
+
+    return (
+        <div className="container-fluid">
+            <div className="todoList">
+                <div className="header">Today's Task</div>
+                <NewTodo
+                    onAddItem={handleAddTodo}
+                />
+                <Sort
+                    category={selectedCategory}
+                    onCategoryChange={handleCategoryChange}
+                />
+                <ul className="todos">
+
+                    {todosToDisplay.map((todo) => (
+                        <Todo
+                            key={todo.id}
+                            todo={todo}
+                            onUpdateItem={handleUpdateItem}
+                            onItemDelete={handleDeleteItem}
+                        />
+                    ))}
+
+                </ul>
+            </div>
+        </div>
+    );
+}
+
+export default TodoList;
