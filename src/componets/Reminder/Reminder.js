51554b00de2b9d1553a10edaca13f7dc5cd936f0
Felix <barosiofelix@gmail.com>
2022-11-11 00:01:43 +0300

    Final commit

--- src/componets/Reminder/Reminder.js
+++ src/componets/Reminder/Reminder.js
@@ -3,56 +3,65 @@ import { data } from "./Birthday-data";
 import "./Reminder.css";
 
 function Reminder() {
-  const [people, setPeople] = useState(data)
-  const [userName, setUserName] = useState([])
+  const [people, setPeople] = useState(data);
+  const [userName, setUserName] = useState([]);
 
-  const handleChange =(e)=>{setUserName(e.target.value);}
+  const handleChange = (e) => {
+    setUserName(e.target.value);
+  };
   const removePerson = (id) => {
-    let newPerson = people.filter((person) => person.id !== id)
-    setPeople(newPerson)
-  }
+    let newPerson = people.filter((person) => person.id !== id);
+    setPeople(newPerson);
+  };
 
   return (
-    <>
-  
+    <div>
       <h3
         style={{
-          backgroundColor: '#fff',
-          color: '#000000',
-          textAlign: 'center',
-          fontSize: '35px',
-          marginTop: '50px'
+          backgroundColor: "#fff",
+          color: "#000000",
+          textAlign: "center",
+          fontSize: "35px",
+          marginTop: "50px",
         }}
       >
-        {people.length} Pending Birthdays 
+        {people.length} Pending Birthdays
       </h3>
       <form>
-  <label>
-    <input type="text" value={userName} required onChange={(e) => {handleChange(e)}}/>
-  </label>
+        <label>
+          <input
+            type="text"
+            value={userName}
+            required
+            onChange={(e) => {
+              handleChange(e);
+            }}
+          />
+        </label>
 
-  <button className='button' onClick={()=>setUserName(userName)}>Add Birthday</button>
-</form>
+        <button className="button" onClick={() => setUserName(userName)}>
+          Add Birthday
+        </button>
+      </form>
 
       {people.map((person) => {
-        const { id, name, image } = person
+        const { id, name, image } = person;
 
         return (
-    
-          <div className='container' key={id}>
-            <div className='avatar'>
+          <div className="container" key={id}>
+            <div className="avatar">
               <img src={image} alt={name} />
               <h3>{name}</h3>
             </div>
-            <div className='button'>
-              <button className='btn' onClick={() => removePerson(id)}>
+            <div className="button">
+              <button className="btn delete" onClick={() => removePerson(id)}>
                 Delete
               </button>
             </div>
           </div>
-        )
+        );
       })}
-    </>
-  )
+    </div>
+  );
 }
 export default Reminder;
